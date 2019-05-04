c7d2d116f16c398e728310a39dc08f5422151d1e
brian120488 <brian.yang@csedge.org>
2019-05-04 17:38:46 -0400

    Updated Pokemon API

--- pokemon-starter/scripts.js
+++ pokemon-starter/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+
 	$("#btn1").click(getPokemon1);
+	$("#btn2").click(getPokemon2);
 
 	function getPokemon1() {
 		let id = Math.floor(Math.random() * 800);
@@ -7,22 +9,22 @@ $(document).ready(function(){
 			console.log(response);
 			$("#first").empty();
 			$("#first").append(`<img src=${response.sprites.front_default} height=100px width=100px>`);
-			for(let i = 0; i < 6; i++) {
-				$("#first").append(`<p>${response.stats[i].stat.name.charAt(0).toUpperCase} + ${response.stats[i].stat.name.substring(0,).toUpperCase}: ${response.stats[0].base_stat}</p>`);
+			for(let i = 5; i >= 0; i--) {
+				let name = response.stats[i].stat.name;
+				$("#first").append(`<p>${name.charAt(0).toUpperCase()}${name.substring(1,name.length)}: ${response.stats[i].base_stat}</p>`);
 			}
-
 		});
-
 	}
-
-	$("#btn2").click(getPokemon2);
 
 	function getPokemon2() {
 		let id = Math.floor(Math.random() * 800);
 		$.get(`https://pokeapi.co/api/v2/pokemon/${id}/`, function(response) {
 			$("#second").empty();
-			$("#second").append(`<img src=${response.sprites.front_default} height=100px width=100px>
-								 <p>Speed: ${response.stats[0].base_stat}</p>`);
+			$("#second").append(`<img src=${response.sprites.front_default} height=100px width=100px>`);
+			for(let i = 5; i >= 0; i--) {
+				let name = response.stats[i].stat.name;
+				$("#second").append(`<p>${name.charAt(0).toUpperCase()}${name.substring(1,name.length)}: ${response.stats[i].base_stat}</p>`);
+			}
 		});
 	}
 })
